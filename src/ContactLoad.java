f850581e4424a8c849cd18f323d715fe326a474a
Jesse <Jdorman1289@gmail.com>
2023-08-10 15:00:46 -0500

    finish first method

---
+++ src/ContactLoad.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactLoad {
+
+    public static ArrayList<Contacts> contactLoad(){
+        String directory = "data";
+        String filename = "contacts.txt";
+        List<String> contactList;
+
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
+
+        if (Files.notExists(dataDirectory)) {
+            try {
+                Files.createDirectories(dataDirectory);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if (! Files.exists(dataFile)) {
+            try {
+                Files.createFile(dataFile);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        Path contactsPath = Paths.get("data", "contacts.txt");
+        try {
+            contactList = Files.readAllLines(contactsPath);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        ArrayList<Contacts> theContactObs = new ArrayList<>();
+        for (int i = 0; i < contactList.size(); i += 1) {
+//            System.out.println((i + 1) + ": " + contactList.get(i));
+            String[] arrOfStr = contactList.get(i).split(" ", 2 );
+
+            Contacts contact = new Contacts(arrOfStr[0],arrOfStr[1]);
+            theContactObs.add(contact);
+
+        }
+
+        return theContactObs;
+    }
+}
--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -1,11 +1,7 @@
 //package src;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class ContactsManager {
 
@@ -14,7 +10,7 @@ public class ContactsManager {
 
 //
 //        Load all of the contacts by calling a method that returns a List of Contact objects.
-        ArrayList<Contacts> thePeeps = contactLoad();
+        ArrayList<Contacts> thePeeps = ContactLoad.contactLoad();
         for(Contacts person : thePeeps) {
             System.out.println(person.getName());
         }
@@ -31,51 +27,5 @@ public class ContactsManager {
 
 
     }
-    public static ArrayList<Contacts> contactLoad(){
-        String directory = "data";
-        String filename = "contacts.txt";
-        List<String> contactList;
-
-
-
-        Path dataDirectory = Paths.get(directory);
-        Path dataFile = Paths.get(directory, filename);
-
-        if (Files.notExists(dataDirectory)) {
-            try {
-                Files.createDirectories(dataDirectory);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        if (! Files.exists(dataFile)) {
-            try {
-                Files.createFile(dataFile);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        Path contactsPath = Paths.get("data", "contacts.txt");
-        try {
-            contactList = Files.readAllLines(contactsPath);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        ArrayList<Contacts> theContactObs = new ArrayList<>();
-        for (int i = 0; i < contactList.size(); i += 1) {
-//            System.out.println((i + 1) + ": " + contactList.get(i));
-            String[] arrOfStr = contactList.get(i).split(" ", 2 );
-
-            Contacts contact = new Contacts(arrOfStr[0],arrOfStr[1]);
-            theContactObs.add(contact);
-
-        }
-
-        return theContactObs;
-    }
-
-
 
 }
