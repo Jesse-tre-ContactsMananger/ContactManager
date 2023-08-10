eda46577de64832653221a3369bf819c0a78d125
Jesse <Jdorman1289@gmail.com>
2023-08-10 15:29:18 -0500

    finish main menu

---
+++ src/ContactMainMethod.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ContactMainMethod {
+
+    public static int contactMainMethod() {
+        int userInput = 0;
+        Scanner sc = new Scanner(System.in);
+
+        while (userInput != 1 && userInput != 2 && userInput != 3 && userInput != 4 && userInput != 5) {
+            String menu = """
+                    1. View contacts.
+                    2. Add a new contact.
+                    3. Search a contact by name.
+                    4. Delete an existing contact.
+                    5. Exit.
+                    Enter an option (1, 2, 3, 4 or 5):
+                    """;
+            System.out.println(menu);
+            userInput = sc.nextInt();
+        }
+
+        sc.close();
+        return userInput;
+    }
+
+
+}
--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -11,12 +11,9 @@ public class ContactsManager {
 //
 //        Load all of the contacts by calling a method that returns a List of Contact objects.
         ArrayList<Contacts> thePeeps = ContactLoad.contactLoad();
-        for(Contacts person : thePeeps) {
-            System.out.println(person.getName());
-        }
 //
 ////        Call a method that shows the user the main menu and returns their choice of action.
-//        contactMainMethod();
+        ContactMainMethod.contactMainMethod();
 ////        Using the user's choice from the previous step, call a method that performs that action
 ////        (modifying the contents of the List of Contact objects if applicable).
 //        runUserChoice();
